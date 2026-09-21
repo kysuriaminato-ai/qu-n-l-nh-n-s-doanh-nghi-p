639b44ce59fa14d0e2200ce9cc91818b3aa5bbc0
kysuriaminato <kysuriaminato@gmail.com>
2026-09-21 13:54:46 +0700

    Update all source code

---
+++ quản lý nhân sự doanh nghiệp/Models/AuthViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace quản_lý_nhân_sự_doanh_nghiệp.Models
+{
+    public class AuthViewModel
+    {
+    }
+}
